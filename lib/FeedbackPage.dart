b360a6c5dd9166df8f7c37f471b3f7052d24cfab
tupac001 <gayana.20200672@iit.ac.lk>
2022-02-17 17:22:03 +0530

    Set the background in feedback page.

--- lib/FeedbackPage.dart
+++ lib/FeedbackPage.dart
@@ -11,7 +11,21 @@ class FeedbackPageState extends StatefulWidget {
 class FeedbackPage extends State<FeedbackPageState> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.cyan, Colors.blueGrey]
+                  )
+              )
+          ),
+        ],
+      ),);
+
   }
 
 }
