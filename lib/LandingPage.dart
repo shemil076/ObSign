e5dc497c96ca64fd4dfe0012cb9315c29012b7c5
Vat2002 <vathila20@outlook.com>
2022-02-17 12:12:58 +0530

    Added the container to the landing page

--- lib/LandingPage.dart
+++ lib/LandingPage.dart
@@ -10,3 +10,33 @@ class LandingPageState extends StatefulWidget {
   State<LandingPageState> createState() => LandingPage();
 
 }
+
+class LandingPage extends State<LandingPageState>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+            children: [
+                Container(
+                decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.cyan, Colors.blueGrey])),
+              ),
+              Center(
+                  child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                  SizedBox(height: 10.0),
+                  Container(
+                  width: 100.0,
+                  height: 100.0,
+                  child: Image(
+                  image: AssetImage('assets/logo/logo_transparent.png'),
+                  ),
+                  ),
+  }
+
+}
