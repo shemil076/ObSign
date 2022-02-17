cb7e412306afd9e0c5c50efab9d5639d6c7db56b
Pramuditha Karunarathna <shemil076@gmail.com>
2022-02-17 18:58:14 +0530

    🚸 set a glass effect to the container.

--- lib/ShowTheSignPage.dart
+++ lib/ShowTheSignPage.dart
@@ -36,6 +36,38 @@ class ShowSignPage extends State<ShowTheSignState> {
                     image: AssetImage('assets/logo/logo_transparent.png'),
                   ),
                 ),
+                Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30)
+                    ),
+                    child: GlassContainer(
+                      height: 550,
+                      width: 300,
+                      borderRadius: BorderRadius.circular(30),
+                      gradient: LinearGradient(
+                          colors:[Colors.white.withOpacity(0.60), Colors.white.withOpacity(0.60)],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight
+                      ),
+                      borderGradient: LinearGradient(
+                        colors: [Colors.white.withOpacity(0.70), Colors.white.withOpacity(0.20), Colors.lightBlueAccent.withOpacity(0.10), Colors.lightBlueAccent.withOpacity(0.12)],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        stops: [0.0, 0.39, 0.40, 1.0],
+                      ),
+                      blur: 15.0,
+                      borderWidth: 1.5,
+                      elevation: 3.0,
+                      isFrostedGlass: true,
+                      shadowColor: Colors.black.withOpacity(0.20),
+                      alignment: Alignment.center,
+                      frostedOpacity: 0.2,
+                      margin: EdgeInsets.all(8.0),
+                      padding: EdgeInsets.all(8.0),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
