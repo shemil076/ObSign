af69968b4b850c96d0d0b289bb6d56787c22b1c8
Ravindu Fernando <ravindu.2019404@iit.ac.lk>
2022-02-19 11:15:28 +0530

    Added the log In screen widgets .

--- lib/LoginScreenPage.dart
+++ lib/LoginScreenPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:glass_kit/glass_kit.dart';
 import 'package:landingpage/LandingPage.dart';
 
+import 'SignUpScreenPage.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -9,4 +10,10 @@ class LoginScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => StartState();
 }
+
+class StartState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return initWidget();
+  }
 
