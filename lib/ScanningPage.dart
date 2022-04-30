f8454cd7032395945294e796faf05bc70d9ff93f
Pramuditha Karunarathna <shemil076@gmail.com>
2022-05-01 02:17:04 +0530

    implemented a class to show the preview of the the captured image or the selected image

--- lib/ScanningPage.dart
+++ lib/ScanningPage.dart
@@ -231,3 +231,107 @@ class _ScanningPage extends State<Home> {
 
 }
 
+class DisplayPictureScreen extends StatelessWidget {
+  final File imagePath;
+  final String  ObjectLabel;
+
+
+
+  const DisplayPictureScreen({Key? key, required this.imagePath, required this.ObjectLabel }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.cyan, Colors.blueGrey])),
+              ),
+              Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 10.0),
+                        const SizedBox(
+                          width: 100.0,
+                          height: 100.0,
+                          child: Image(
+                            image: AssetImage('assets/logo/logo_transparent.png'),
+                          ),
+                        ),
+                        Center(
+                            child: Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30)),
+                                child: GlassContainer(
+                                    height: 550,
+                                    width: 300,
+                                    borderRadius: BorderRadius.circular(30),
+                                    gradient: LinearGradient(
+                                        colors: [
+                                          Colors.white.withOpacity(0.60),
+                                          Colors.white.withOpacity(0.60)
+                                        ],
+                                        begin: Alignment.topLeft,
+                                        end: Alignment.bottomRight),
+                                    borderGradient: LinearGradient(
+                                      colors: [
+                                        Colors.white.withOpacity(0.70),
+                                        Colors.white.withOpacity(0.20),
+                                        Colors.lightBlueAccent.withOpacity(0.10),
+                                        Colors.lightBlueAccent.withOpacity(0.12)
+                                      ],
+                                      begin: Alignment.topLeft,
+                                      end: Alignment.bottomRight,
+                                      stops: const [0.0, 0.39, 0.40, 1.0],
+                                    ),
+                                    blur: 15.0,
+                                    borderWidth: 1.5,
+                                    elevation: 3.0,
+                                    isFrostedGlass: true,
+                                    shadowColor: Colors.black.withOpacity(0.20),
+                                    alignment: Alignment.center,
+                                    frostedOpacity: 0.2,
+                                    margin: const EdgeInsets.all(8.0),
+                                    padding: const EdgeInsets.all(8.0),
+
+                                    child: Column(
+                                        children: [
+                                          const SizedBox(height: 70),
+                                          Center(
+                                              child: Image.file(imagePath)
+                                          ),
+                                          const SizedBox(height: 20),
+
+                                          RaisedButton(onPressed:()async {
+
+
+                                          }, color: Colors.lightBlue[400],
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.circular(25.0),
+                                              ),
+                                              child: const Text(
+                                                "Show Sign",
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              )),
+                                        ]
+                                    )
+                                )
+                            )
+                        )
+                      ]
+                  )
+              )
+            ]
+        )
+    );
+  }
+}
