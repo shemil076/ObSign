cb13785b271217377bfe1d81705c49b901944659
Pramuditha Karunarathna <shemil076@gmail.com>
2022-05-01 01:43:19 +0530

    implementing the scanning page

---
+++ lib/ScanningPage.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:glass_kit/glass_kit.dart';
+import 'LandingPage.dart';
+import 'ShowTheSignPage.dart';
+
+
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _ScanningPage createState() => _ScanningPage();
+}
+
+class _ScanningPage extends State<Home> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
+
