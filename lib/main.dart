d7d30dca94f75852cb126f36e3b5558f0d159bc8
Vat2002 <vathila20@outlook.com>
2022-05-02 01:26:11 +0530

    Commented on code fragments

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,11 @@ import 'ScanningPage.dart';
 import 'ShowTheSignPage.dart';
 import 'SplashScreenPage.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+Future<void> main() async { //perform asynchronous operations
+  WidgetsFlutterBinding.ensureInitialized(); //calls the constructor of the binding class it is the class that adapts the dart ui
 
   runApp(MaterialApp(
+    //initializing the routes for the pages in the application
     initialRoute: '/',
     routes: {
       '/': (context) => const SplashScreen(),
@@ -23,13 +24,13 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);  // forwards to the constructor of the super class and passes the parameter key passed to MyApp to the super constructors key parameter
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      debugShowCheckedModeBanner: false, //turns off the debug mode in the application
+      home: SplashScreen(),  //open the application's splash screen when the application is launched
     );
   }
 }
